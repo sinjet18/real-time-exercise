d31ebedec18b8f5ba9915f303ee2b1c94a3867c6
sinjet18 <sinwongjet@gmail.com>
2025-05-17 23:17:51 +0800

    Add files via upload

---
+++ TestAtomicInteger1p.java
@@ -0,0 +1,39 @@
+package week8;
+
+import java.util.concurrent.atomic.AtomicInteger; // New modification: Import AtomicInteger
+
+class TestAtomicInteger1p {
+    public static void main(String[] args) throws InterruptedException {
+        CountProblem pt = new CountProblem();
+        Thread t1 = new Thread(pt, "t1");
+        Thread t2 = new Thread(pt, "t2");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("Count=" + pt.getCount());
+    }
+}
+
+class CountProblem implements Runnable {
+
+    private AtomicInteger count = new AtomicInteger(0); // New modification: Use AtomicInteger to prevent race condition
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            processSomething(i);
+            count.incrementAndGet(); // New modification: Increse the count num 1 by 1
+        }
+    }
+    public int getCount() {
+        return count.get();//New modification: using get to retrieve the count value
+    }
+    private void processSomething(int i) {
+        try {
+            Thread.sleep(i * 200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
