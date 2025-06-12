c6d2a7a499aa199f4b05ccdb77ea2a310710a85f
sinjet18 <sinwongjet@gmail.com>
2025-06-12 23:47:16 +0800

    Add files via upload

---
+++ week6/MyVolatile.java
@@ -0,0 +1,40 @@
+package week6;
+
+import java.util.Scanner;
+
+class MyThread extends Thread {
+    private volatile boolean running = true;
+
+    public void run() {
+        while (running) {
+            System.out.println("Thread is running...");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public void shutdown() {
+        running = false;
+        this.interrupt();
+    }
+}
+
+public class MyVolatile {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+
+        new Scanner(System.in).nextLine(); // Wait for Enter key
+        thread.shutdown();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
